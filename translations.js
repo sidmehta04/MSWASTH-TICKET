409d5db1574f94b7b2e4467d9a6c2752aedee44a
sidmehta04 <116738018+sidmehta04@users.noreply.github.com>
2024-07-01 22:42:52 +0530

    Update translations.js

--- translations.js
+++ translations.js
@@ -1,234 +1,127 @@
-function _0x3229(_0x1428f3, _0x5c51ed) {
-  const _0x1c77cc = _0x1c77();
-  return (
-    (_0x3229 = function (_0x322974, _0x3096c4) {
-      _0x322974 = _0x322974 - 0xfc;
-      let _0x3854b1 = _0x1c77cc[_0x322974];
-      return _0x3854b1;
-    }),
-    _0x3229(_0x1428f3, _0x5c51ed)
-  );
-}
-const _0xc5c824 = _0x3229;
-(function (_0x27b8b8, _0x3016aa) {
-  const _0x39cda9 = _0x3229,
-    _0x248d73 = _0x27b8b8();
-  while (!![]) {
-    try {
-      const _0x2aef45 =
-        -parseInt(_0x39cda9(0x12e)) / 0x1 +
-        -parseInt(_0x39cda9(0x127)) / 0x2 +
-        (-parseInt(_0x39cda9(0xfe)) / 0x3) *
-          (-parseInt(_0x39cda9(0x12f)) / 0x4) +
-        -parseInt(_0x39cda9(0x11b)) / 0x5 +
-        (parseInt(_0x39cda9(0x131)) / 0x6) *
-          (-parseInt(_0x39cda9(0x124)) / 0x7) +
-        -parseInt(_0x39cda9(0x137)) / 0x8 +
-        (parseInt(_0x39cda9(0x123)) / 0x9) * (parseInt(_0x39cda9(0x113)) / 0xa);
-      if (_0x2aef45 === _0x3016aa) break;
-      else _0x248d73["push"](_0x248d73["shift"]());
-    } catch (_0x599110) {
-      _0x248d73["push"](_0x248d73["shift"]());
-    }
-  }
-})(_0x1c77, 0x4b1aa);
 const translations = {
-  en: {
-    "header-title": _0xc5c824(0x12c),
-    dcName: _0xc5c824(0x11f),
-    employeeIdSearch: "Search\x20Employee\x20ID",
-    employeeId: _0xc5c824(0x105),
-    submit1: "Submit",
-    partnerNameSearch: _0xc5c824(0x138),
-    addItemButton: _0xc5c824(0xfc),
-    clearFormButton: _0xc5c824(0x117),
-    instructionsButton: _0xc5c824(0x144),
-    instruction: _0xc5c824(0x122),
-    a: _0xc5c824(0x121),
-    b: _0xc5c824(0x120),
-    c: _0xc5c824(0x12a),
-    d: "Click\x20on\x20\x22For\x20any\x20Doubt\x20Pin\x20me\x20on\x20the\x20group\x20or\x20connect\x20with\x20your\x20manager\x22",
-    e: "Click\x20on\x20\x22If\x20your\x20Employee\x20ID\x20is\x20missing\x20you\x20can\x20select\x20the\x20other\x20option\x20there\x20it\x20will\x20auto\x20add\x20the\x20new\x20one\x20for\x20you\x22",
-    f: _0xc5c824(0x10b),
-    g: _0xc5c824(0x13a),
-    "toggle-button": "Toggle\x20Dark\x20Mode",
-    clinicCodeInput: _0xc5c824(0x133),
-    quantityInput: _0xc5c824(0x119),
-    priceInput: _0xc5c824(0x10d),
-    sumInput: _0xc5c824(0x114),
-    deleteItem: _0xc5c824(0x101),
-    duplicateItem: "Duplicate",
-  },
-  hi: {
-    "header-title": _0xc5c824(0x13d),
-    dcName: _0xc5c824(0x141),
-    employeeIdSearch: "कर्मचारी\x20आईडी\x20खोजें",
-    employeeId: _0xc5c824(0x126),
-    submit1: _0xc5c824(0x134),
-    partnerNameSearch: "साझेदार\x20का\x20नाम",
-    addItemButton: _0xc5c824(0x12b),
-    clearFormButton: _0xc5c824(0x13f),
-    instructionsButton: _0xc5c824(0x129),
-    instruction: _0xc5c824(0x129),
-    a: "आइटम\x20जोड़ने\x20के\x20लिए\x20\x27एक\x20और\x20आइटम\x20जोड़ें\x27\x20पर\x20क्लिक\x20करें।",
-    b: _0xc5c824(0x139),
-    c: _0xc5c824(0x10c),
-    d: _0xc5c824(0x118),
-    e: _0xc5c824(0x132),
-    f: _0xc5c824(0x11a),
-    g: _0xc5c824(0x112),
-    "toggle-button": _0xc5c824(0x125),
-    clinicCodeInput: _0xc5c824(0x10f),
-    quantityInput: "मात्रा",
-    priceInput: _0xc5c824(0x136),
-    sumInput: _0xc5c824(0x12d),
-    deleteItem: _0xc5c824(0x102),
-    duplicateItem: "डुप्लिकेट",
-  },
-  bn: {
-    "header-title": "এম-সওয়াস্থ\x20ফার্নিচার\x20স্টোর\x20পৃষ্ঠায়\x20স্বাগতম",
-    dcName: "ডিসি\x20নাম",
-    employeeIdSearch: "কর্মচারী\x20আইডি\x20সন্ধান\x20করুন",
-    employeeId: _0xc5c824(0x110),
-    submit1: _0xc5c824(0x116),
-    partnerNameSearch: "অংশীদারের\x20নাম",
-    addItemButton: "আরেকটি\x20আইটেম\x20যোগ\x20করুন",
-    clearFormButton: _0xc5c824(0x111),
-    instructionsButton: _0xc5c824(0x13c),
-    instruction: _0xc5c824(0x13c),
-    a: _0xc5c824(0x106),
-    b: _0xc5c824(0x100),
-    c: "\x22মূল্য\x22\x20এ,\x20প্রতি\x20পরিমাণ,\x20প্রতি\x20ইউনিট\x20মূল্য\x20প্রবেশ\x20করুন।",
-    d: _0xc5c824(0x107),
-    e: _0xc5c824(0x115),
-    f: _0xc5c824(0x142),
-    g: _0xc5c824(0x143),
-    "toggle-button": "ডার্ক\x20মোড\x20টগল\x20করুন",
-    clinicCodeInput: _0xc5c824(0x104),
-    quantityInput: _0xc5c824(0x10a),
-    priceInput: _0xc5c824(0x11e),
-    sumInput: "মোট",
-    deleteItem: "আইটেম\x20মুছুন",
-    duplicateItem: _0xc5c824(0x128),
-  },
+    en: {
+        'header-title': 'WELCOME TO THE M-Swasth FURNITURE STORE PAGE',
+        'dcName': 'DC Name',
+        'employeeIdSearch': 'Search Employee ID',
+        'employeeId': 'Select Employee ID',
+        'submit1': 'Submit',
+        'partnerNameSearch': 'Search Partner Name',
+        // 'partnerName': 'Select Partner Name',
+        'addItemButton': 'Add Another Item',
+        'clearFormButton': 'Clear Form',
+        "instructionsButton": "Instructions",
+        "instruction": "INSTRUCTIONS",
+        "a": "Click on 'Add Another Item' to add items.",
+        "b": "Click on 'Any type of file can be uploaded here but make sure 1 file is selected'",
+        "c": 'In "Price", enter the per quantity, per unit price.',
+        "d": 'Click on "For any Doubt Pin me on the group or connect with your manager"',
+        "e": 'Click on "If your Employee ID is missing you can select the other option there it will auto add the new one for you"',
+        "f": 'In "Item", specify the quantity purchased it will auto capture the sum amount.',
+        "g": 'In "Employee ID", select from the available IDs. If not available, it will automatically select "Other".',
+        "toggle-button": "Toggle Dark Mode",
+        "clinicCodeInput": "Clinic Code",
+        "quantityInput": "Quantity",
+        "priceInput": "Price",
+        "sumInput": "Sum",
+        "deleteItem": "Delete Item",
+        "duplicateItem": "Duplicate",
+        // Add more translations here
+    },
+    hi: {
+        'header-title': 'एम-स्वस्थ फर्नीचर स्टोर पेज में आपका स्वागत है',
+        'dcName': 'डीसी का नाम',
+        'employeeIdSearch': 'कर्मचारी आईडी खोजें',
+        'employeeId': 'कर्मचारी आईडी चुनें',
+        'submit1': 'जमा करें',
+        'partnerNameSearch': 'साझेदार का नाम',
+        
+        'addItemButton': 'एक और आइटम जोड़ें',
+        'clearFormButton': 'फॉर्म साफ करें',
+        "instructionsButton": "निर्देश",
+        "instruction": "निर्देश",
+        "a": "आइटम जोड़ने के लिए 'एक और आइटम जोड़ें' पर क्लिक करें।",
+        "b": "यहां कोई भी प्रकार की फ़ाइल अपलोड की जा सकती है, लेकिन सुनिश्चित करें कि केवल एक फ़ाइल का चयन किया गया है।",
+        "c": '"कीमत" में, प्रति मात्रा, प्रति इकाई मूल्य दर्ज करें।',
+        "d": "कोई संदेह होने पर समूह में पिन करें या अपने प्रबंधक से संपर्क करें।",
+        "e": "यदि आपका 'कर्मचारी आईडी गायब है, तो आप वहां दूसरा विकल्प चुन सकते हैं और यह स्वचालित रूप से नया जोड़ देगा।",
+        "f": '"आइटम" में, खरीदी गई मात्रा निर्दिष्ट करें, यह स्वचालित रूप से कुल राशि को कैप्चर कर लेगा।',
+        "g": '"कर्मचारी आईडी" में, उपलब्ध आईडी में से चुनें। यदि उपलब्ध नहीं है, तो यह स्वचालित रूप से "अन्य" का चयन करेगा।',
+        "toggle-button": "डार्क मोड टॉगल करें",
+        "clinicCodeInput": "क्लिनिक कोड",
+        "quantityInput": "मात्रा",
+        "priceInput": "कीमत",
+        "sumInput": "योग",
+        "deleteItem": "आइटम हटाएं",
+        "duplicateItem": "डुप्लिकेट",
+        // Add more translations here
+    },
+    bn: {
+        'header-title': 'এম-সওয়াস্থ ফার্নিচার স্টোর পৃষ্ঠায় স্বাগতম',
+        'dcName': 'ডিসি নাম',
+        'employeeIdSearch': 'কর্মচারী আইডি সন্ধান করুন',
+        'employeeId': 'কর্মচারী আইডি নির্বাচন করুন',
+        'submit1': 'জমা দিন',
+        'partnerNameSearch': 'অংশীদারের নাম',
+        'addItemButton': 'আরেকটি আইটেম যোগ করুন',
+        'clearFormButton': 'ফর্ম পরিষ্কার করুন',
+        "instructionsButton": "নির্দেশাবলী",
+        "instruction": "নির্দেশাবলী",
+        "a": "'আরেকটি আইটেম যোগ করুন' এ ক্লিক করুন আইটেম যোগ করতে।",
+        "b": "যেকোন ধরনের ফাইল এখানে আপলোড করা যাবে, কিন্তু নিশ্চিত করুন যে শুধুমাত্র একটি ফাইল নির্বাচন করা হয়েছে।",
+        "c": '"মূল্য" এ, প্রতি পরিমাণ, প্রতি ইউনিট মূল্য প্রবেশ করুন।',
+        "d": "যে কোন সন্দেহ থাকলে গ্রুপে পিন করুন বা আপনার ম্যানেজারের সাথে যোগাযোগ করুন।",
+        "e": "যদি আপনার EMP_ID অনুপস্থিত থাকে, তাহলে আপনি সেখানে অন্য বিকল্প নির্বাচন করতে পারেন এবং এটি স্বয়ংক্রিয়ভাবে নতুনটি যোগ করবে।",
+        "f": '"আইটেম" এ, কেনা পরিমাণ নির্দিষ্ট করুন, এটি স্বয়ংক্রিয়ভাবে মোট পরিমাণ ধারণ করবে।',
+        "g": '"EMP ID" এ, উপলব্ধ আইডি থেকে নির্বাচন করুন। যদি উপলব্ধ না থাকে, তবে এটি স্বয়ংক্রিয়ভাবে "অন্য" নির্বাচন করবে।',
+        "toggle-button": "ডার্ক মোড টগল করুন",
+        "clinicCodeInput": "ক্লিনিক কোড",
+        "quantityInput": "পরিমাণ",
+        "priceInput": "মূল্য",
+        "sumInput": "মোট",
+        "deleteItem": "আইটেম মুছুন",
+        "duplicateItem": "ডুপ্লিকেট",
+        // Add more translations here
+    }
 };
-document[_0xc5c824(0x135)]("input[name=\x22language\x22]")["forEach"](
-  (_0x4f7e8d) => {
-    _0x4f7e8d["addEventListener"]("change", function () {
-      const _0x2dc7f6 = _0x3229;
-      translatePage(this[_0x2dc7f6(0xfd)]);
+
+
+document.querySelectorAll('input[name="language"]').forEach((elem) => {
+    elem.addEventListener('change', function() {
+        translatePage(this.value);
     });
-  }
-);
-function translatePage(_0x4150fa) {
-  const _0x9ed67 = _0xc5c824,
-    _0x324baf = document[_0x9ed67(0x135)]("[id],\x20[class]");
-  _0x324baf[_0x9ed67(0x10e)]((_0x28c688) => {
-    const _0x138bb1 = _0x9ed67,
-      _0x1f7198 = [];
-    _0x28c688["id"] && _0x1f7198[_0x138bb1(0x11d)](_0x28c688["id"]),
-      _0x28c688[_0x138bb1(0x108)][_0x138bb1(0x10e)]((_0x1349e6) => {
-        const _0x46815c = _0x138bb1;
-        _0x1f7198[_0x46815c(0x11d)](_0x1349e6);
-      }),
-      _0x1f7198[_0x138bb1(0x10e)]((_0x45d3a9) => {
-        const _0x336058 = _0x138bb1,
-          _0xdeb0d0 = translations[_0x4150fa][_0x45d3a9];
-        if (_0xdeb0d0) {
-          if (
-            _0x28c688[_0x336058(0xff)][_0x336058(0x11c)]() ===
-              _0x336058(0x13b) ||
-            _0x28c688[_0x336058(0xff)][_0x336058(0x11c)]() === _0x336058(0x109)
-          )
-            _0x28c688["placeholder"] = _0xdeb0d0;
-          else
-            _0x28c688[_0x336058(0xff)][_0x336058(0x11c)]() ===
-              _0x336058(0x103) && _0x28c688["id"] === _0x336058(0x140)
-              ? (_0x28c688["options"][0x0][_0x336058(0x13e)] = _0xdeb0d0)
-              : (_0x28c688[_0x336058(0x130)] = _0xdeb0d0);
+});
+
+
+function translatePage(language) {
+    const elementsToTranslate = document.querySelectorAll('[id], [class]');
+
+    elementsToTranslate.forEach(element => {
+        const translationKeys = [];
+        
+        // Add the id to the list of keys if it exists
+        if (element.id) {
+            translationKeys.push(element.id);
         }
-      });
-  });
+
+        // Add each class name to the list of keys
+        element.classList.forEach(cls => {
+            translationKeys.push(cls);
+        });
+
+        // Loop through all potential keys to find a matching translation
+        translationKeys.forEach(translationKey => {
+            const translation = translations[language][translationKey];
+            if (translation) {
+                if (element.tagName.toLowerCase() === 'input' || element.tagName.toLowerCase() === 'textarea') {
+                    element.placeholder = translation;
+                } else if (element.tagName.toLowerCase() === 'select' && element.id === 'employeeId') {
+                    element.options[0].text = translation;
+                } else {
+                    element.innerHTML = translation;
+                }
+            }
+        });
+    });
 }
-translatePage("en");
-function _0x1c77() {
-  const _0x29aa94 = [
-    "8kRCjLQ",
-    "innerHTML",
-    "738wbBhuY",
-    "यदि\x20आपका\x20\x27कर्मचारी\x20आईडी\x20गायब\x20है,\x20तो\x20आप\x20वहां\x20दूसरा\x20विकल्प\x20चुन\x20सकते\x20हैं\x20और\x20यह\x20स्वचालित\x20रूप\x20से\x20नया\x20जोड़\x20देगा।",
-    "Clinic\x20Code",
-    "जमा\x20करें",
-    "querySelectorAll",
-    "कीमत",
-    "2843752mVcuiZ",
-    "Search\x20Partner\x20Name",
-    "यहां\x20कोई\x20भी\x20प्रकार\x20की\x20फ़ाइल\x20अपलोड\x20की\x20जा\x20सकती\x20है,\x20लेकिन\x20सुनिश्चित\x20करें\x20कि\x20केवल\x20एक\x20फ़ाइल\x20का\x20चयन\x20किया\x20गया\x20है।",
-    "In\x20\x22Employee\x20ID\x22,\x20select\x20from\x20the\x20available\x20IDs.\x20If\x20not\x20available,\x20it\x20will\x20automatically\x20select\x20\x22Other\x22.",
-    "input",
-    "নির্দেশাবলী",
-    "एम-स्वस्थ\x20फर्नीचर\x20स्टोर\x20पेज\x20में\x20आपका\x20स्वागत\x20है",
-    "text",
-    "फॉर्म\x20साफ\x20करें",
-    "employeeId",
-    "डीसी\x20का\x20नाम",
-    "\x22আইটেম\x22\x20এ,\x20কেনা\x20পরিমাণ\x20নির্দিষ্ট\x20করুন,\x20এটি\x20স্বয়ংক্রিয়ভাবে\x20মোট\x20পরিমাণ\x20ধারণ\x20করবে।",
-    "\x22EMP\x20ID\x22\x20এ,\x20উপলব্ধ\x20আইডি\x20থেকে\x20নির্বাচন\x20করুন।\x20যদি\x20উপলব্ধ\x20না\x20থাকে,\x20তবে\x20এটি\x20স্বয়ংক্রিয়ভাবে\x20\x22অন্য\x22\x20নির্বাচন\x20করবে।",
-    "Instructions",
-    "Add\x20Another\x20Item",
-    "value",
-    "522393FOGLXm",
-    "tagName",
-    "যেকোন\x20ধরনের\x20ফাইল\x20এখানে\x20আপলোড\x20করা\x20যাবে,\x20কিন্তু\x20নিশ্চিত\x20করুন\x20যে\x20শুধুমাত্র\x20একটি\x20ফাইল\x20নির্বাচন\x20করা\x20হয়েছে।",
-    "Delete\x20Item",
-    "आइटम\x20हटाएं",
-    "select",
-    "ক্লিনিক\x20কোড",
-    "Select\x20Employee\x20ID",
-    "\x27আরেকটি\x20আইটেম\x20যোগ\x20করুন\x27\x20এ\x20ক্লিক\x20করুন\x20আইটেম\x20যোগ\x20করতে।",
-    "যে\x20কোন\x20সন্দেহ\x20থাকলে\x20গ্রুপে\x20পিন\x20করুন\x20বা\x20আপনার\x20ম্যানেজারের\x20সাথে\x20যোগাযোগ\x20করুন।",
-    "classList",
-    "textarea",
-    "পরিমাণ",
-    "In\x20\x22Item\x22,\x20specify\x20the\x20quantity\x20purchased\x20it\x20will\x20auto\x20capture\x20the\x20sum\x20amount.",
-    "\x22कीमत\x22\x20में,\x20प्रति\x20मात्रा,\x20प्रति\x20इकाई\x20मूल्य\x20दर्ज\x20करें।",
-    "Price",
-    "forEach",
-    "क्लिनिक\x20कोड",
-    "কর্মচারী\x20আইডি\x20নির্বাচন\x20করুন",
-    "ফর্ম\x20পরিষ্কার\x20করুন",
-    "\x22कर्मचारी\x20आईडी\x22\x20में,\x20उपलब्ध\x20आईडी\x20में\x20से\x20चुनें।\x20यदि\x20उपलब्ध\x20नहीं\x20है,\x20तो\x20यह\x20स्वचालित\x20रूप\x20से\x20\x22अन्य\x22\x20का\x20चयन\x20करेगा।",
-    "13800guoKYe",
-    "Sum",
-    "যদি\x20আপনার\x20EMP_ID\x20অনুপস্থিত\x20থাকে,\x20তাহলে\x20আপনি\x20সেখানে\x20অন্য\x20বিকল্প\x20নির্বাচন\x20করতে\x20পারেন\x20এবং\x20এটি\x20স্বয়ংক্রিয়ভাবে\x20নতুনটি\x20যোগ\x20করবে।",
-    "জমা\x20দিন",
-    "Clear\x20Form",
-    "कोई\x20संदेह\x20होने\x20पर\x20समूह\x20में\x20पिन\x20करें\x20या\x20अपने\x20प्रबंधक\x20से\x20संपर्क\x20करें।",
-    "Quantity",
-    "\x22आइटम\x22\x20में,\x20खरीदी\x20गई\x20मात्रा\x20निर्दिष्ट\x20करें,\x20यह\x20स्वचालित\x20रूप\x20से\x20कुल\x20राशि\x20को\x20कैप्चर\x20कर\x20लेगा।",
-    "3400taRwox",
-    "toLowerCase",
-    "push",
-    "মূল্য",
-    "DC\x20Name",
-    "Click\x20on\x20\x27Any\x20type\x20of\x20file\x20can\x20be\x20uploaded\x20here\x20but\x20make\x20sure\x201\x20file\x20is\x20selected\x27",
-    "Click\x20on\x20\x27Add\x20Another\x20Item\x27\x20to\x20add\x20items.",
-    "INSTRUCTIONS",
-    "8244EloXWJ",
-    "25396UjLbQE",
-    "डार्क\x20मोड\x20टॉगल\x20करें",
-    "कर्मचारी\x20आईडी\x20चुनें",
-    "217370SEtBBV",
-    "ডুপ্লিকেট",
-    "निर्देश",
-    "In\x20\x22Price\x22,\x20enter\x20the\x20per\x20quantity,\x20per\x20unit\x20price.",
-    "एक\x20और\x20आइटम\x20जोड़ें",
-    "WELCOME\x20TO\x20THE\x20M-Swasth\x20FURNITURE\x20STORE\x20PAGE",
-    "योग",
-    "393638PXYJHd",
-  ];
-  _0x1c77 = function () {
-    return _0x29aa94;
-  };
-  return _0x1c77();
-}
+
+translatePage('en');
